781a5054c0a5383e5ae4d61ad006f6c952bdabf9
jvitor <jvitor.pinheiro.garcia@gmail.com>
2025-04-24 16:21:24 -0300

    oi

--- escolaProj/src/main/java/com/escola/escolaProj/dto/AlunoDTO.java
+++ escolaProj/src/main/java/com/escola/escolaProj/dto/AlunoDTO.java
@@ -7,8 +7,43 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
 public class AlunoDTO {
+    private Long idAluno;
+    private String nome;
+    private String cpf;
+    private Long turma_id;
 
 
+ 
 
+    public Long getIdAluno() {
+        return idAluno;
+    }
 
+    public void setIdAluno(Long idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getTurma_id() {
+        return turma_id;
+    }
+
+    public void setTurma_id(Long turma_id) {
+        this.turma_id = turma_id;
+    }
 }
