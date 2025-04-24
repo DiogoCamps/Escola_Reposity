871464673eba00c8d9868b75618c30bddca96ffc
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-04-24 14:42:37 -0300

    classe turma

---
+++ escolaProj/src/main/java/com/escola/escolaProj/Entity/Turma.java
@@ -0,0 +1,26 @@
+package com.escola.escolaProj.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Turma implements Serializable {
+     @Id
+     @GeneratedValue
+     private Long id;
+     private String sigla;
+     private Integer numeroSala;
+     private String nome;
+     private Professor professor;
+     private Aluno aluno;
+
+
+}
