826eac9ca771554a6503e0229eb60c2a994c95bc
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-04-24 15:17:34 -0300

    TurmaRepository criado

---
+++ escolaProj/src/main/java/com/escola/escolaProj/repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package com.escola.escolaProj.repository;
+
+import com.escola.escolaProj.Entity.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+}
