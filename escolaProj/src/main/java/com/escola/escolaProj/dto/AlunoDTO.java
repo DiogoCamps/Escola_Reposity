df6a934526aee6371a061a9823fa30d8ed506141
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-04-24 15:11:13 -0300

    atualização repository

--- escolaProj/src/main/java/com/escola/escolaProj/dto/AlunoDTO.java
+++ escolaProj/src/main/java/com/escola/escolaProj/dto/AlunoDTO.java
@@ -1,4 +1,14 @@
 package com.escola.escolaProj.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 public class AlunoDTO {
+
+
+
+
 }
---
+++ escolaProj/src/main/java/com/escola/escolaProj/dto/TurmaDTO.java
@@ -0,0 +1,42 @@
+package com.escola.escolaProj.dto;
+
+import com.escola.escolaProj.Entity.Turma;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TurmaDTO implements Serializable {
+    private Long id;
+    private String sigla;
+    private Integer numeroSala;
+    private String nome;
+    private Professor professor;
+    private Aluno aluno;
+
+  public Turma toTurma(){
+      return new Turma(
+              this.id = id,
+              this.sigla = sigla,
+              this.numeroSala = numeroSala,
+              this.nome = nome,
+              this.professor = professor,
+              this.aluno = aluno
+      );
+  }
+      public TurmaDTO fromTurma(Turma turma){
+          return new TurmaDTO(
+                  turma.getId(),
+                  turma.getSigla(),
+                  turma.getNumeroSala(),
+                  turma.getNome(),
+                  turma.getProfessor(),
+                  turma.getAluno()
+          );
+  }
+
+}
