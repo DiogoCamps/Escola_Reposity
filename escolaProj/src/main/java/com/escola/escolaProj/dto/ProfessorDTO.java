ec06bd3e759ef8a86a27eb33733f421ce0d693d5
Gabriel-August <gabrielasnc55@gmail.com>
2025-04-30 15:02:57 -0300

    tt

--- escolaProj/src/main/java/com/escola/escolaProj/dto/ProfessorDTO.java
+++ escolaProj/src/main/java/com/escola/escolaProj/dto/ProfessorDTO.java
@@ -27,12 +27,12 @@ public class ProfessorDTO implements Serializable {
         );
     }
 
-    public ProfessorDTO fromProfessor(Professor professor){
+    public static ProfessorDTO fromProfessor(Professor professor){
         return new ProfessorDTO(
                 professor.getId(),
                 professor.getNome(),
                 professor.getSobrenome(),
-                professor.getTurmas()   ,
+                professor.getTurmas()
         );
 
     }
